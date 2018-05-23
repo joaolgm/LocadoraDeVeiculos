59cc1679064006f1bc82126c9925ffa7cf4e8ffd
Oliveiros Oliveira <oliveiros.oliveira@virtus.com>
2018-05-23 08:48:38 -0300

    Substituição dos printf e scanf por cin e cout

--- README.md
+++ README.md
@@ -4,7 +4,4 @@ Projeto PLP 2018.1
 
 Alunos:
 
-Oliveiros Neto
-João Lucas
-Gilvan Neto
-Wesley Lucena
+Oliveiros Neto, João Lucas, Gilvan Neto, Wesley Lucena e Jonas Aguiar.
--- main.cpp
+++ main.cpp
@@ -11,60 +11,63 @@ int main(){
     int MAXVEHICLES = 50; 
     string id, year, type, model, state;
     
-    printf("Bem vindo!\n");
-    printf("Selecione uma das opções abaixo:\n");
-    printf("1 - Adicionar veículo ao inventário.\n");
-    printf("2 - Remover veículo ao inventário.\n");
-    printf("3 - Visualizar veículo do inventário.\n");
-    printf("4 - Atualizar veículo do inventário.\n");
-    printf("5 - Listar todos os veículos do inventário.\n");
+    cout << "Bem vindo!" << endl;
     
-    scanf("%d",&option);
+    do{
+        cout << "Selecione uma das opções abaixo:" << endl;
+        cout << "1 - Adicionar veículo ao inventário." << endl;
+        cout << "2 - Remover veículo ao inventário." << endl;
+        cout << "3 - Visualizar veículo do inventário." << endl;
+        cout << "4 - Atualizar veículo do inventário." << endl;
+        cout << "5 - Listar todos os veículos do inventário." << endl;
+        cout << "6 - Sair" << endl;
 
-    switch (option){
-        case 1:
-            if (vehicles > MAXVEHICLES) {
-                printf("O inventário está cheio.\n");
-            } else {
-                printf("Digite o id do veículo a ser adicionado:\n");
-                scanf("%s",&id);
-                printf("Digite o ano de fabricação do veículo a ser adicionado:\n");
-                scanf("%s",&year);
-                printf("Digite o tipo do veículo a ser adicionado:\n");
-                scanf("%s",&type);
-                printf("Digite o modelo do veículo a ser adicionado:\n");
-                scanf("%s",&model);
-                addVehicle(id, year, type, model);
-                vehicles++;
-            }           
-            break;
-        case 2:
-            if(vehicles == 0){
-                printf("O inventário está vazio.\n");
-            } else {
-                printf("Digite o id do veículo a ser removido:\n");
-                scanf("%s",&id);
-                removeVehicle(id);
-                vehicles--;
-            }           
-            break;
-        case 3:            
-            printf("Digite o id do veículo a ser visualizado:\n");
-            scanf("%s",&id);
-            viewVehicle(id);
-            break;
-        case 4:            
-            printf("Digite o id do veículo a ser atualizado:\n");
-            scanf("%s",&id);
-            printf("O estado atual do veículo é %s \n", returnState(id));
-            printf("O novo estado do veículo deve ser:\n");
-            scanf("%s",&state);
-            updateVehicle(id, state);
-            break;
-        case 5:            
-            viewAll();
-            break;
-    }
+        cin >> option;
 
-
+        switch (option){
+            case 1:
+                if (vehicles > MAXVEHICLES) {
+                    cout << "O inventário está cheio." << endl;
+                } else {
+                    cout << "Digite o id do veículo a ser adicionado:" << endl;
+                    cin >> id;
+                    cout << "Digite o ano de fabricação do veículo a ser adicionado:" << endl;
+                    cin >> year;
+                    cout << "Digite o tipo do veículo a ser adicionado:" << endl;
+                    cin >> type;
+                    cout << "Digite o modelo do veículo a ser adicionado:" << endl;
+                    cin >> model;
+                    addVehicle(id, year, type, model);
+                    vehicles++;
+                }
+                break;
+            case 2:
+                if(vehicles == 0){
+                    cout << "O inventário está vazio." << endl;
+                } else {
+                    cout << "Digite o id do veículo a ser removido:" << endl;
+                    cin >> id;
+                    removeVehicle(id);
+                    vehicles--;
+                }
+                break;
+            case 3:
+                cout << "Digite o id do veículo a ser visualizado:" << endl;
+                cin >> id;
+                viewVehicle(id);
+                break;
+            case 4:
+                cout << "Digite o id do veículo a ser atualizado:" << endl;
+                cin >> id;
+                cout << "O estado atual do veículo é: " << returnState(id) << endl;
+                cout << "O novo estado do veículo deve ser:" << endl;
+                cin >> state;
+                updateVehicle(id, state);
+                break;
+            case 5:
+                viewAll();
+                break;
+        }
+    
+    } while(option != 6);
 }
